0b52d04d12fc50e7830cc0349b4d34ac53ddcf21
UICHEOL-HWANG <icuchoel@gmail.com>
2022-09-11 23:53:41 +0900

    localstorage

---
+++ localstorage.js
@@ -0,0 +1,13 @@
+// localStorage.setItem("myName",myName)// 영구적으로 저장되어 있는 상태 크롬 어플리케이션 로컬스토리지 칸에 
+
+// const myName = localStorage.getItem("myNmame")
+
+// alert(myName)
+
+// localStorage.setItem("cat","고양이")
+// localStorage.setItem("dog","강아지")
+// localStorage.setItem("myName","의철") 영구적으로 스토리지에 저장됨 
+
+// localStorage.removeItem("cat") // 스토리지 안에 cat이 사라짐 
+
+// localStorage.clear()// 모든 스토리지 안에 있는 인자들이 다 사라짐 
